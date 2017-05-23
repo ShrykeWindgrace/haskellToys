be04e1ccd49209845f119476ef6138b6785a53c9
Timofey ZAKREVSKIY <timofey.zakrevskiy@3ds.com>
2017-05-23 16:05:29 +0200

    Some more experiments with parsing strings and lines

--- app/Main.hs
+++ app/Main.hs
@@ -6,10 +6,13 @@ import Qs
 import Text.Parsec
 main :: IO ()
 main = --go
-  case  parse parseQ "" "? asd ert?" of
-    Right answ -> print $ "OK: " ++ answ
-    Left err -> print err
-  {-putStrLn $ printField $ Just "ok"-}
+  do
+    ln <- readFile "input.txt"
+    print ln
+    let lns = lines ln
+    mapM_ someF1 lns -- sequence_ $ map someF1 lns== mapM_ someF1 lns
+    mapM_ ( print . length ) lns
+ {-putStrLn $ printField $ Just "ok"-}
 
 
 go :: IO ()
@@ -20,6 +23,13 @@ go = do
     putStrLn "Bye"
   else
     do
-      someFunc s
+      someF1 s
       go
  
+
+
+someF1 str =  case  parse combiQ "" str of
+    Right answ -> print $ "OK: " ++ answ
+    Left err -> print err
+ 
+qGrammar = many 
--- src/Qs.hs
+++ src/Qs.hs
@@ -1,4 +1,4 @@
-module Qs (parseQ) where
+module Qs (parseQ, combiQ, blankLine) where
 
 import Text.Parsec
 import Text.Parsec.String
@@ -7,7 +7,22 @@ qText :: Parser String
 qText = do
   char '?'
   spaces 
-  s <- many anyChar--alphaNum
-  return s
+  s <- regText
+  return $ "Q:" ++ s
 
 parseQ = qText
+
+regText :: Parser String
+regText = many1 anyChar -- >>= return "RT"
+   
+
+combiQ :: Parser String
+combiQ = parseQ <|> blankLine <|> regText <|> newline1
+
+blankLine :: Parser String
+blankLine = do
+  char ' '
+  {-endOfLine  -}
+  return "blank"
+
+newline1 = (newline >> return "blanche") <?> "wtf"
