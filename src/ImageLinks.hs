62101e3760d91afb7fbf49eb0f885bf21c85dd98
Timofey ZAKREVSKIY <timofey.zakrevskiy@3ds.com>
2017-05-29 16:43:50 +0200

    Skeleton for image links parsing

---
+++ src/ImageLinks.hs
@@ -0,0 +1,45 @@
+module ImageLinks
+    (
+        imageLink
+    )
+
+where
+
+import Text.Parsec
+import Text.Parsec.String
+import Control.Monad (void)
+
+
+sizeParse :: Char -> Parser Integer
+sizeParse c = do
+    void $ char c
+    spaces
+    void $ char '='
+    spaces
+    s <- many1 digit
+    void $ string "px"
+    return $ read s
+
+
+widParse :: Parser Integer
+widParse = sizeParse 'w'
+
+heiParse :: Parser Integer
+heiParse = sizeParse 'h'
+
+
+linkContents :: Parser ()
+linkContents = do
+    spaces
+    optional widParse
+    spaces
+    optional heiParse
+    spaces
+    void $ many1 $ noneOf ")\n"
+
+
+-- (img w=20px h=40px kotik.jpg)
+imageLink :: Parser Char
+imageLink = do
+    between (string "(img")  (char ')') linkContents
+    return '&'
