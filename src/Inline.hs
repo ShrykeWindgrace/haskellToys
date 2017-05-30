7a69394e689943ee0b373a9a6aada8770e11485e
TZ-DELL\Timofey <timofey.zakrevskiy@gmail.com>
2017-05-30 22:50:16 +0200

    Several implementations of the same thing; the latter parser is used for testing

--- src/Inline.hs
+++ src/Inline.hs
@@ -7,5 +7,11 @@ import Text.Parsec.String
 
 emphText :: Parser String
 emphText = do
-  s <- between (char '_') (char '_') $ many1 $ noneOf "\n"
+  s <- between (char '_') (char '_') $ many1 $ noneOf "_\n"
   return $ "EMPH:" ++ s
+
+emphText' :: Parser String
+emphText' = (++ "EMPH:") <$> between (char '_') (char '_') (many1 $ noneOf "_\n")
+
+emphText'' :: Parser Char
+emphText'' = const 'E' <$> between (char '_') (char '_') (many1 $ noneOf "_\n")
