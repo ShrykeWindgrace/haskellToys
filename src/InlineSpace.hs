5b570c5c9159e5c27397a249f2f2c09b38af88a9
Timofey ZAKREVSKIY <timofey.zakrevskiy@3ds.com>
2017-06-14 14:55:57 +0200

    Limit imports to minimum

--- src/InlineSpace.hs
+++ src/InlineSpace.hs
@@ -1,11 +1,9 @@
 module InlineSpace
     (spaces')
-
 where
 
-import Text.Parsec
-import Text.Parsec.String
-import Control.Monad (void)
+import Text.Parsec (many, oneOf)
+import Text.Parsec.String (Parser)
 
 
 -- If we ever want to deal with other whitespace characters, we should implement this parser in the same spirit as "isSpace"
