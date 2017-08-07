b72a8af46f5a75015380bd2d6cd6295b3425f1f6
DSONE\TZY1 <timofey.zakrevskiy@3ds.com>
2017-08-08 00:11:26 +0200

    Move ImageLinks UTs to a separate file

---
+++ test/ImageLinkSpec.hs
@@ -0,0 +1,32 @@
+module ImageLinkSpec where
+
+import           ImageLinks
+import           Test.Hspec
+import           Text.Parsec
+
+parseHelper = parse imageLink' ""
+
+
+imSpec = do
+    describe "(img test.jpg)" $ do
+        it "should be well parsed to \"test.jpg\"" $
+            (parseHelper "(img test.jpg)") `shouldBe` (Right $ ILink "test.jpg" Nothing Nothing)
+
+    describe "(img h=400px test.jpg)" $ do
+        it "should be well-parsed to link and height" $
+            (parseHelper "(img h=400px test.jpg)") `shouldBe` (Right $ ILink "test.jpg" Nothing (Just 400))
+
+    describe "(img w=600px test.jpg)" $ do
+        it "should be well-parsed to link and width" $
+            (parseHelper "(img w=600px test.jpg)") `shouldBe` (Right $ ILink "test.jpg" (Just 600) Nothing)
+
+    describe "(img w=600px h = 400px test.jpg)" $ do
+        it "should be well-parsed to link and width" $
+            (parseHelper "(img w=600px h = 400px test.jpg)") `shouldBe` (Right $ ILink "test.jpg" (Just 600) (Just 400))
+
+
+    describe "(img h=400px w = 600px test.jpg)" $ do
+        it "should be well-parsed to link and width" $
+            (parseHelper "(img h=400px w = 600px test.jpg)") `shouldBe` (Right $ ILink "test.jpg" (Just 600) (Just 400))
+
+
