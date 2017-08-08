2c445e9c9657dd1461e63a3ce3c523556cd370bd
Timofey ZAKREVSKIY <timofey.zakrevskiy@3ds.com>
2017-08-08 20:27:28 +0200

    end of the day commit

--- src/InlineSpace.hs
+++ src/InlineSpace.hs
@@ -8,4 +8,4 @@ import Text.Parsec.String (Parser)
 -- If we ever want to deal with other whitespace characters, we should implement this parser in the same spirit as "isSpace"
 -- method in Parsec
 spaces' :: Parser ()
-spaces' = () <$ (many $ oneOf " \t")
+spaces' = () <$ many (oneOf " \t")
--- test/ImageLinkSpec.hs
+++ test/ImageLinkSpec.hs
@@ -3,13 +3,52 @@ module ImageLinkSpec
 
 where
 
+import           Data.Maybe
 import           ImageLinks
 import           Test.Hspec
 import           Text.Parsec
 
+parseHelper :: String -> Either ParseError ILink
 parseHelper = parse imageLink' ""
 
+linkText :: String
+linkText = "test.jpg"
 
+showMaybe :: Maybe Integer -> String
+showMaybe Nothing  = ""
+showMaybe (Just n) = show n
+
+
+showWidth :: ILink -> String
+showWidth iLink
+    | isNothing (width iLink) = ""
+    | otherwise = "w = " ++ showMaybe (width iLink) ++ "px"
+
+showHeight :: ILink -> String
+showHeight iLink
+    | isNothing (height iLink) = ""
+    | otherwise = "w = " ++ showMaybe (height iLink) ++ "px"
+
+
+
+linkToParseable :: ILink -> String
+linkToParseable iLink = "(img " ++ showWidth iLink ++ showHeight iLink ++ link iLink ++ ")"
+
+dataSet :: [ILink]
+dataSet = [ILink linkText w h | w <- [Nothing, Just 600], h <- [Nothing, Just 400]]
+
+dataSetString ::[String]
+dataSetString = linkToParseable <$> dataSet
+
+shoulders :: [Expectation]
+shoulders = zipWith shouldBe (parseHelper <$> dataSetString) (Right <$> dataSet)
+
+specs = (\x ->  describe x  (it "should be ok")) <$> dataSetString 
+
+imSpec2 ::[Spec]
+imSpec2 = zipWith ($) specs shoulders
+
+imSpec :: Spec
 imSpec = do
     describe "(img test.jpg)" $
         it "should be well parsed to \"test.jpg\"" $
