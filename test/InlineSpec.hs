3cf894355cd981e1d8697d9664766824eb4ee430
Timofey ZAKREVSKIY <timofey.zakrevskiy@3ds.com>
2017-08-17 15:33:45 +0200

    one more test case

--- test/InlineSpec.hs
+++ test/InlineSpec.hs
@@ -19,7 +19,7 @@ dssgr :: [String]
 dssgr = ["удар<str>е</str>ние", "<str>е</str>"]
 
 dataSetStressBad :: [String]
-dataSetStressBad = ["", "`", "сомелье`"]
+dataSetStressBad = ["", "`", "сомелье`", "нет"]
 
 shoulds :: [Expectation]
 shoulds = zipWith shouldBe (parseHelper <$> dataSetStressGood) (Right <$> dssgr)
