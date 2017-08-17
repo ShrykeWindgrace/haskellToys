10c7a1d695ecde2d0ec0c8de300f06cd96d1a53d
Timofey ZAKREVSKIY <timofey.zakrevskiy@3ds.com>
2017-08-17 14:41:15 +0200

    add more tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,7 @@
 import           ImageLinks
+import           InlineSpec
 import           ImageLinkSpec
 import           Test.Hspec
 import           Text.Parsec
 
-main = hspec imSpec2 >> hspec imSpec
+main = hspec imSpec2 >> hspec imSpec >> hspec inlineSpec
