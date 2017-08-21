c0290a6594be7216f3f99ae6869a3f4145dfc1cd
DSONE\TZY1 <timofey.zakrevskiy@3ds.com>
2017-08-21 16:24:45 +0200

    Remove dead code

--- test/Helpers.hs
+++ test/Helpers.hs
@@ -6,8 +6,3 @@ import           Text.Parsec.String
 
 parseGen :: Parser a -> String -> Either ParseError a
 parseGen _parser = parse _parser ""
-
-
-isLeft :: Either a b -> Bool
-isLeft (Right _) = False
-isLeft (Left _)  = True
