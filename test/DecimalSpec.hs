f8b99bf8ea7cdaa64c84f9b68034a17513f223b0
Timofey ZAKREVSKIY <timofey.zakrevskiy@3ds.com>
2017-08-22 10:56:57 +0200

    reorganise property

--- test/DecimalSpec.hs
+++ test/DecimalSpec.hs
@@ -1,15 +1,20 @@
-module DecimalSpec where
+module DecimalSpec (spec) where
 
-import Helpers
-import Inline
-import Test.QuickCheck
-import Test.Hspec
+import           Data.Either     (isLeft)
+import           Helpers         (parseGen)
+import           Inline          (decimal)
+import           Test.Hspec
+import           Test.QuickCheck
+import           Text.Parsec
 
-parserHelper = parseGen decimal
+parserHelper :: Integer -> Either ParseError Integer
+parserHelper = parseGen decimal . show
 
 
+tester :: Integer -> Bool
+tester x
+    | x < 0 = isLeft $ parserHelper x
+    | otherwise = Right x == parserHelper x
 
-spec = describe "decimal parser" $ do
-    it "should correctly parse positive integers" $
-        property $
-            \x -> parserHelper (show (abs x)) == Right ((abs x) :: Integer)
+spec = describe "decimal parser" $ it "should correctly parse positive integers" $
+    property $ \x -> tester (x:: Integer)
