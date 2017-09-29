e466e36d4324e5304e598021b8fed3dac99f1691
Timofey Zakrevskiy <timofey.zakrevskiy@gmail.com>
2017-09-29 15:50:54 +0200

    Codestyle in shellscripts

--- makeGraph.sh
+++ makeGraph.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 echo 'run stack and dot; take all dependencies, even external'
 stack dot --external | dot -Tpdf -o deps.pdf
--- makeStructure.sh
+++ makeStructure.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 find src -name '*.hs' | xargs graphmod -q -p | dot -Tpdf -o structure.pdf
