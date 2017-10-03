3c4187ded4313be0f08486f5a0c4895f9bed912e
ShrykeWindgrace <shryke.windgrace@gmail.com>
2017-10-03 23:20:33 +0200

    crude handmade grammar testing

---
+++ test/JQSpec.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE RecordWildCards #-}
+module JQSpec (spec) where
+
+
+{-}
+import           Control.Monad      (zipWithM_)
+import           Data.Maybe
+import           Helpers
+import           Parsers.ImageLinks
+import           Structures.Words
+import           Test.Hspec
+
+
+-}
+import           Constants.StringWorks
+import           Data.Maybe             (fromJust)
+import           Parsers.InlineSpace
+import           Parsers.Lines
+import           Parsers.QN
+import           Structures.Lines
+import           Structures.QNumber
+import           Structures.Quest
+import           Structures.Words
+
+import           Test.Hspec
+import           Test.QuickCheck
+import           Text.Megaparsec
+import           Text.Megaparsec.String
+
+
+spec :: Spec
+spec = describe "placeholder" $ it "placeholder" $ (parse testGrammar "" testLine) `shouldBe` (Right expectedResult)
+
+testLine :: String
+testLine = "? вышел ОН из\nтумана\n№ ноль\n! (img w = 20px h =   40px moon.jpg) месяц\n!= moon"
+
+
+pLine' = pLine <* char '\n'
+
+
+testGrammar :: Parser Question
+testGrammar = do
+    string (parsingToken QText)
+    skipSpaces
+    l1 <- pLine'
+    l2 <- pLine'
+    modifierM <- questModifier
+    char '\n'
+    let modifier = fromJust modifierM
+    string (parsingToken QAnswer)
+    skipSpaces
+    l3 <- pLine'
+    string (parsingToken QNotEquiv)
+    skipSpaces
+    l4 <- pLine
+    let fields = QField QText [l1, l2] : QField QAnswer [l3] : QField QNotEquiv [l4] : []
+    return Question{..}
+
+
+expectedResult :: Question
+expectedResult = Question {
+    modifier = Soft "ноль",
+    fields = QField QText [Line [RegWord "вышел", RegWord "ОН", RegWord "из"], Line [RegWord "тумана"] ] : QField QAnswer [Line[ILinkStr $ ILink "moon.jpg" (Just 20) (Just 40)], Line [RegWord "месяц"]] : QField QNotEquiv [Line [RegWord "moon"]] : []
+    }
