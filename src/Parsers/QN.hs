4d312e09d17276b9f4f7a0c6b4988b801a0241d2
ShrykeWindgrace <shryke.windgrace@gmail.com>
2017-10-03 23:20:02 +0200

    crude handmade grammar testing

--- src/Parsers/QN.hs
+++ src/Parsers/QN.hs
@@ -8,6 +8,7 @@ import           Constants.StringWorks  (parsingToken)
 import           Parsers.InlineSpace    (skipSpaces)
 import           Parsers.Lines          (pLine)
 import           Parsers.Primitives     (decimal)
+import           Structures.Lines
 import           Structures.QNumber     (QModifier (..), QModifierM)
 import           Text.Megaparsec        (optional, string, try, (<|>))
 import           Text.Megaparsec.String (Parser)
@@ -18,8 +19,9 @@ qHardReset = Hard <$> (string (parsingToken $ Hard 0) >> skipSpaces >> decimal)
 
 
 qSoftReset :: Parser QModifier
-qSoftReset = Soft . show <$> (string (parsingToken $ Soft "") >> skipSpaces >> pLine) -- todo: use human-readable version of Show
-
+qSoftReset = Soft . showHumanReadable <$> (string (parsingToken $ Soft "") >> skipSpaces >> pLine) where
+    showHumanReadable :: Line -> String
+    showHumanReadable (Line list) = unwords (show <$> list)
 
 questModifier :: Parser QModifierM
 questModifier = optional $ try qHardReset <|> try qSoftReset
