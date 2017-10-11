393688ca5c6c0fbf57702933cbec9f71c0d60c13
Timofey Zakrevskiy <timofey.zakrevskiy@gmail.com>
2017-10-11 19:19:28 +0200

    ListLines will be seen as a single line

--- src/Structures/Lines.hs
+++ src/Structures/Lines.hs
@@ -3,14 +3,14 @@ module Structures.Lines where
 import           Structures.Words (OneWord)
 -- import           Constants.StringWorks
 
-newtype Line = Line [OneWord] deriving (Eq, Show) -- TODO rename to avoid "qualified imports" requirement
+data Line = Line [OneWord] | ListLinesStr ListLines deriving (Eq, Show)
 
 -- instance Show Line where
     -- show (Line _words) = unwords (show <$> _words)
 
-instance Monoid Line where
-    mempty = Line []
-    Line lst `mappend` Line lst2 = Line $ lst ++ lst2
+-- instance Monoid Line where
+--     mempty = Line []
+--     Line lst `mappend` Line lst2 = Line $ lst ++ lst2
     -- this instance supposes that these lines are not separated; there are no newlines here
 
 
