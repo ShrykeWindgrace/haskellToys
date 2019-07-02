9dbcfc0beaecbb5f71d25bfe089d898e2fc1e6ec
Timofey Zakrevskiy <timofey.zakrevskiy@gmail.com>
2019-07-02 15:41:10 +0200

    pass with hlint

--- test/HumanReadableSpec.hs
+++ test/HumanReadableSpec.hs
@@ -5,11 +5,11 @@ import           Structures.QNumber    (QModifier (..))
 import           Structures.Quest      (QFieldType (..))
 import           Test.Hspec            (Spec, describe, it, shouldBe)
 spec :: Spec
-spec = do
+spec =
     describe "Rendering" $ do
         it "Вопрос" $ do
             showNatural (Soft "") `shouldBe` "Вопрос"
             showNatural (Hard 0) `shouldBe` "Вопрос"
         it "Ответ" $ do
-            showNatural (QAnswer) `shouldBe` "Ответ"
-            showNatural (QEquiv) `shouldBe` "Зачёт"
+            showNatural QAnswer `shouldBe` "Ответ"
+            showNatural QEquiv `shouldBe` "Зачёт"
